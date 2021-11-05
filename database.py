e9b827b6ad8259e1871fbefb9166a169b8794c6a
calebag <93626186+calebag@users.noreply.github.com>
2021-11-04 20:31:23 -0700

    Redid and (for now) finished tags

--- database.py
+++ database.py
@@ -12,11 +12,13 @@ def exit(connection):
 
 def create_entry_table():
     conn = connect()
+    conn.execute("PRAGMA foreign_keys = 1")
     c = conn.cursor()
 
     with conn:
         c.execute('''CREATE TABLE IF NOT EXISTS log_entries (
-            callsign TEXT,
+            id INTEGER PRIMARY KEY NOT NULL,
+            callsign TEXT NOT NULL,
             name TEXT,
             date TEXT,
             time TEXT,
@@ -32,35 +34,47 @@ def create_entry_table():
         )''')
 
 
-def create_tags_table(name):
-    """
-    TODO: - finish and test tags system
-          - use foreign key to add key table to main entry table
-          - not necessarily a problem since this is offline, but look into better
-            practices than " + name + ", can cause sql injection problems
-    """
-
+def create_tags_table():
     conn = connect()
+    conn.execute("PRAGMA foreign_keys = 1")
     c = conn.cursor()
 
     with conn:
-        c.execute("CREATE TABLE IF NOT EXISTS tags_"+name+" (tags TEXT)")
+        c.execute("CREATE TABLE IF NOT EXISTS tags_table (id INTEGER PRIMARY KEY NOT NULL, tag TEXT)")
 
 
-def tag_insert(name, tags):
+def create_join_table():
     conn = connect()
+    conn.execute("PRAGMA foreign_keys = 1")
     c = conn.cursor()
 
     with conn:
-        c.executemany("INSERT INTO " + name + " VALUES(?)", tags)
+        c.execute('''CREATE TABLE IF NOT EXISTS log_tag_join (
+        log_id INTEGER,
+        tag_id INTEGER,
+        FOREIGN KEY(log_id) REFERENCES log_entries(id),
+        FOREIGN KEY(tag_id) REFERENCES tags_table(id)
+        )''')
+
+
+def tag_insert(tags):
+    conn = connect()
+    conn.execute("PRAGMA foreign_keys = 1")
+    c = conn.cursor()
+
+    tags = zip(iter(tags))
+
+    with conn:
+        c.executemany("INSERT INTO tags_table(tag) VALUES(?)", tags)
 
 
 def insert(entry):
     conn = connect()
+    conn.execute("PRAGMA foreign_keys = 1")
     c = conn.cursor()
 
     with conn:
-        c.execute("INSERT INTO log_entries VALUES (:callsign, :name, :date, :time, :mode, :band, :country, :state, :city, :coord_x, :coord_y, :talk_group, :other)",
+        c.execute("INSERT INTO log_entries(callsign, name, date, time, mode, band, country, state, city, coord_x, coord_y, talk_group, other) VALUES (:callsign, :name, :date, :time, :mode, :band, :country, :state, :city, :coord_x, :coord_y, :talk_group, :other)",
                   {'callsign': entry.callsign,
                    'name': entry.name,
                    'date': entry.date,
@@ -77,6 +91,14 @@ def insert(entry):
                    })
 
 
+def log_tag_insert(log_id, tag_id):
+    conn = connect()
+    conn.execute("PRAGMA foreign_keys = 1")
+    c = conn.cursor()
+
+    c.execute("INSERT INTO log_tag_join(log_id, tag_id) VALUES (:log_id, :tag_id)", (log_id, tag_id))
+
+
 def show_entries():
     conn = connect()
     c = conn.cursor()
@@ -86,10 +108,19 @@ def show_entries():
         print(c.fetchall())
 
 
-def show_tags(name):
+def show_tags():
     conn = connect()
     c = conn.cursor()
 
     with conn:
-        c.execute("SELECT rowid, * FROM " + name)
+        c.execute("SELECT * FROM tags_table")
+        print(c.fetchall())
+
+
+def show_log_tag_join():
+    conn = connect()
+    c = conn.cursor()
+
+    with conn:
+        c.execute("SELECT tag FROM log_entries CROSS JOIN tags_table")
         print(c.fetchall())
--- main_cli.py
+++ main_cli.py
@@ -3,18 +3,21 @@ from entry import Entry
 
 
 def main():
-    db.create_entry_table()
     thing = Entry('KA1ABC', 'Person', '10/29/2021', '22:53', 'FM', '2M', 'USA', 'CA', 'San Francisco', [2.2, 1.1], '', '', ['test', 'example'])
+    # print(thing)
+    db.create_entry_table()
+
     db.insert(thing)
     db.show_entries()
 
-    name = thing.callsign  # Table's name = entry's callsign
-    tags = ["test", "ok", "yeah"]
-    tags = list(zip(*[iter(tags)]))  # turn tags list into list of tuples for SQL
+    tags = thing.tags
 
-    db.create_tags_table(name)
-    db.tag_insert("tags_" + name, tags)
-    db.show_tags("tags_" + name)
+    db.create_tags_table()
+    db.tag_insert(tags)
+    db.show_tags()
+
+    db.create_join_table()
+    db.show_log_tag_join()
 
 
 if __name__ == '__main__':
